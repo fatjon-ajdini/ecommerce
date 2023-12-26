a9bcbeb485174cd08cdbe2876fa12d434db91646
Fatjon Ajdini <fatjonajdini11@gmail.com>
2023-12-26 22:34:18 +0100

    inital load

--- main.js
+++ main.js
@@ -26,31 +26,64 @@
 
 fetch('https://dummyjson.com/products/')
 .then(response => response.json())
-.then(shop => renderProducts(shop.products));
+// .then(shop => renderProducts(shop.products));
+.then(shop => loadIntialProducts(shop));
 
-const cardsContainer = document.querySelector("#cards-container");
+const cardsContainer = document.getElementById("products-container");
+const loadMoreButton = document.getElementsByClassName("btn-load-more");
 
-function renderProducts(products) {
-    products.forEach(product => {
-        const div = document.createElement('div');
-        const image = document.createElement('img');
-        const name = document.createElement('h3');
-        const price = document.createElement('h5');
-        const brand = document.createElement('h5');
+const initialItems = 21;
+const loadItems = 3;
 
-        div.classList = 'card';
-        image.classList = 'card-img';
-        name.classList = 'title';
 
-        image.src = product.thumbnail;
-        name.innerText = product.title;
-        price.innerText = `Price: $${product.price}`;
-        brand.innerText = `Brand: ${product.brand}`;
+function loadIntialProducts(shop) {
+    let out = "";
+    let counter = 0;
+    let products = shop.products;
 
-        div.appendChild(image);
-        div.appendChild(name);
-        div.appendChild(price);
-        div.appendChild(brand);
-        cardsContainer.appendChild(div);
-    });
+    for(let product of products) {
+        if(counter < initialItems) {
+            out += `
+                <div class="card">
+                    <img class="card-img" src="${product.thumbnail}" alt="" style="width:100%">
+                    <div class="container">
+                        <h3 class"title>${product.title}</h3>
+                        <h5>Price: $${product.price}</h5>
+                        <h5>Brand: ${product.brand}</h5>
+                    </div>
+                </div>
+            `;
+        }
+        counter++;
+    }
+    let div = document.createElement("div");
+    div.classList = 'card-products';
+    cardsContainer.prepend(div);
+    div.innerHTML = out;
 };
+
+
+// function renderProducts(products) {
+//     products.forEach(product => {
+//         const div = document.createElement('div');
+//         const image = document.createElement('img');
+//         const name = document.createElement('h3');
+//         const price = document.createElement('h5');
+//         const brand = document.createElement('h5');
+
+//         div.classList = 'card';
+//         image.classList = 'card-img';
+//         name.classList = 'title';
+
+//         image.src = product.thumbnail;
+//         name.innerText = product.title;
+//         price.innerText = `Price: $${product.price}`;
+//         brand.innerText = `Brand: ${product.brand}`;
+
+//         div.appendChild(image);
+//         div.appendChild(name);
+//         div.appendChild(price);
+//         div.appendChild(brand);
+//         cardsContainer.appendChild(div);
+//     });
+// };
