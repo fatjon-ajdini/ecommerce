5f041189198c0e26d9987e2c1848f7d59f314e26
Fatjon Ajdini <fatjonajdini11@gmail.com>
2023-12-27 19:05:52 +0100

    load more

--- main.js
+++ main.js
@@ -22,18 +22,17 @@
 // }
 
 // var timer = setInterval(changeSlide, interval);
+const api = 'https://dummyjson.com/products/';
 
-
-fetch('https://dummyjson.com/products/')
-.then(response => response.json())
-// .then(shop => renderProducts(shop.products));
-.then(shop => loadIntialProducts(shop));
+fetch(api)
+    .then(response => response.json())
+    .then(shop => loadIntialProducts(shop));
 
 const cardsContainer = document.getElementById("products-container");
-const loadMoreButton = document.getElementsByClassName("btn-load-more");
+const loadMoreButton = document.getElementById("btn-load-more");
 
-const initialItems = 21;
-const loadItems = 3;
+const initialItems = 15;
+const loadItems = 6;
 
 
 function loadIntialProducts(shop) {
@@ -58,32 +57,53 @@ function loadIntialProducts(shop) {
     }
     let div = document.createElement("div");
     div.classList = 'card-products';
-    cardsContainer.prepend(div);
+    cardsContainer.insertBefore(div, loadMoreButton);
     div.innerHTML = out;
+
+    // Loading more products (button - eventListener)
+    loadMoreButton.addEventListener("click", function loadData() {
+        let currentDisplayedItems = document.querySelectorAll(".card").length;
+    
+        let out = "";
+        let counter = 0;
+    
+        for(let product of products) {
+            if(counter >= currentDisplayedItems && counter < loadItems + currentDisplayedItems) {
+                out += `
+                    <div class="card">
+                        <img class="card-img" src="${product.thumbnail}" alt="" style="width:100%">
+                        <div class="container">
+                            <h3 class"title>${product.title}</h3>
+                            <h5>Price: $${product.price}</h5>
+                            <h5>Brand: ${product.brand}</h5>
+                        </div>
+                    </div>
+                `;
+            };
+            counter++;
+        };
+        let div = document.createElement("div");
+        div.classList = 'card-products';
+        cardsContainer.insertBefore(div, loadMoreButton);
+        div.innerHTML = out;
+        div.style.opacity = 0;
+    
+        if(document.querySelectorAll(".card").length == products.length) {
+            loadMoreButton.style.display = "none";
+        };
+        fadeIn(div);
+    });
 };
 
-
-// function renderProducts(products) {
-//     products.forEach(product => {
-//         const div = document.createElement('div');
-//         const image = document.createElement('img');
-//         const name = document.createElement('h3');
-//         const price = document.createElement('h5');
-//         const brand = document.createElement('h5');
-
-//         div.classList = 'card';
-//         image.classList = 'card-img';
-//         name.classList = 'title';
-
-//         image.src = product.thumbnail;
-//         name.innerText = product.title;
-//         price.innerText = `Price: $${product.price}`;
-//         brand.innerText = `Brand: ${product.brand}`;
-
-//         div.appendChild(image);
-//         div.appendChild(name);
-//         div.appendChild(price);
-//         div.appendChild(brand);
-//         cardsContainer.appendChild(div);
-//     });
-// };
+// Disabling loadMoreButton after there are no more products to show 
+function fadeIn(div) {
+    let opacity = 0;
+    let interval  = setInterval(function() {
+        if(opacity <= 1) {
+            opacity = opacity + 0.1;
+            div.style.opacity = opacity;
+        } else {
+            clearInterval(interval);
+        }
+    }, 30); 
+};
